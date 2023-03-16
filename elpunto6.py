88cf1dcee7d91e2461497e56fc25b50ec9fe0091
Artuu1999 <124615034+Artuu1999@users.noreply.github.com>
2023-03-15 21:47:56 -0500

    Add files via upload

---
+++ elpunto6.py
@@ -0,0 +1,13 @@
+a = str(input("Ingrese una letra: "))
+if a == "a" or a == "A":
+    print("la letra "+str(a)+" es una vocal")
+elif a == "e" or a == "E":
+    print("la letra "+str(a)+" es una vocal")
+elif a == "i" or a == "I":
+    print("la letra "+str(a)+" es una vocal")
+elif a == "o" or a == "O":
+    print("la letra "+str(a)+" es una vocal")
+elif a == "u" or a == "U":
+    print("la letra "+str(a)+" es una vocal")
+else:
+    print("la letra "+str(a)+" es una consonante")

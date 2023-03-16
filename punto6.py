159ac6b0d7a247db5f7a505ba4b71ad35d6d00e2
Artuu1999 <124615034+Artuu1999@users.noreply.github.com>
2023-03-15 19:23:28 -0500

    Delete punto6.py

--- punto6.py
+++
@@ -1,5 +0,0 @@
-a = input ("ingrese una letra")
-if a in ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']:
- print("la letra "+str(a)+" es una vocal")
-else:
- print ("la letra "+str(a)+" es una consonante")
